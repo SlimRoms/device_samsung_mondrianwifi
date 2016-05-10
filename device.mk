c570a62f9150e5246f605c7b021056cb3cc572e4
Bruno Martins <bgcngm@gmail.com>
2016-05-10 04:26:03 -0700

    msm8974-common: Update WLAN configuration
    * Based on latest M stock package
    
    Change-Id: I49677d688be4e89e4197001133244233b0a72838
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -134,15 +134,22 @@ PRODUCT_PACKAGES += \
     wcnss_service \
     wpa_supplicant
 
+# Wifi config files
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-   $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+   $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+   $(LOCAL_PATH)/configs/hostapd.accept:system/etc/hostapd/hostapd.accept \
+   $(LOCAL_PATH)/configs/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+   $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+PRODUCT_COPY_FILES += \
+
 
 # Common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)

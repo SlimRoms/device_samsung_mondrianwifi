bc17793b0a203da15483ab8dcc272cb72259601f
Christopher R. Palmer <crpalmer@gmail.com>
2016-08-31 05:41:13 -0400

    mondrianwifi: Remove our local copy of wpa_supplicant.conf
    Change-Id: I096f2da64faf24b16408390e3564356d32d1aa8b

--- device.mk
+++ device.mk
@@ -135,7 +135,6 @@ PRODUCT_PACKAGES += \
 
 # Wifi config files
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
    $(LOCAL_PATH)/configs/hostapd.accept:system/etc/hostapd/hostapd.accept \
